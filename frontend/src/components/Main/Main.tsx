8273a23013a65e4e3e860ff74fd3d3ac54182ac5
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-12 12:17:12 +0000

    Add HTML content for main component

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,7 +1,19 @@
+import BookmarkCard from "../BookmarkCard/BookmarkCard";
+
 function Main() {
   return (
     <>
-      <main className="bg-yellow-600 h-full">Main</main>
+      <main className="bg-yellow-600 h-full">
+        <div>
+          <p>All bookmarks</p>
+          <button>
+            <img src="" alt="" /> <p>Sort by</p>
+          </button>
+        </div>
+        <div>
+          <BookmarkCard />
+        </div>
+      </main>
     </>
   );
 }
