7e83b3a7c9571f5378a52a23a03947ce95632684
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-12 12:12:18 +0000

    Add bookmark card component

---
+++ frontend/src/components/BookmarkCard/BookmarkCard.tsx
@@ -0,0 +1,26 @@
+function BookmarkCard() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+        <button>
+            <img src="" alt="" />
+        </button>
+      </div>
+      <div>
+        <p></p>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default BookmarkCard;
