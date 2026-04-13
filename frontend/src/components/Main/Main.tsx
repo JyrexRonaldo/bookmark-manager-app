f954b480978b0ceb6f5889d8aec710e0c584f6be
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-13 13:50:15 +0000

    Add custom checkbox input styling

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -3,7 +3,7 @@ import BookmarkCard from "../BookmarkCard/BookmarkCard";
 function Main() {
   return (
     <>
-      <main className="flex h-full flex-col gap-[20px] bg-[#E8F0EF] px-[16px] py-[24px]">
+      <main className="flex h-full flex-col gap-[20px] bg-[#E8F0EF] px-[16px] pt-[24px] pb-[32px] sm:px-[32px]">
         <div className="flex h-[42px] items-center justify-between">
           <p className="text-[20px]/[120%] font-bold text-[#051513]">
             All bookmarks
@@ -12,7 +12,16 @@ function Main() {
             <img src="/img/icon-sort.svg" alt="" /> <p>Sort by</p>
           </button>
         </div>
-        <div className="flex flex-col gap-[32px] bg-[#E8F0EF]">
+        <div className="grid grid-cols-[repeat(auto-fill,_minmax(338px,_1fr))] gap-[32px]  bg-[#E8F0EF] overflow-y-scroll">
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
+          <BookmarkCard />
           <BookmarkCard />
           <BookmarkCard />
           <BookmarkCard />
--- frontend/src/components/TagComponent/TagComponent.tsx
+++ frontend/src/components/TagComponent/TagComponent.tsx
@@ -2,11 +2,14 @@ function TagComponent() {
   return (
     <>
       <div className="flex h-[38px] items-center justify-between gap-[12px] px-[12px] py-[8px]">
-        <div className="flex grow gap-[8px] items-center">
-          <input type="checkbox" />
+        <div className="flex grow items-center gap-[8px]">
+          <input
+            type="checkbox"
+            className="relative size-[16px] appearance-none rounded-[4px] border border-[#899492] before:invisible before:absolute before:top-[-1px] before:right-[-1px] before:size-[16px] before:rounded-[4px] before:bg-[#014745] before:text-[1.2em] after:invisible after:absolute after:top-[-5px] after:text-white after:content-['✓'] checked:before:visible checked:after:visible"
+          />
           <p className="text-[16px]/[140%] font-semibold">Compatibility</p>
         </div>
-        <p className="flex size-[21px] items-center justify-center rounded-[9999px] border border-[#DDE9E7] bg-[#E8F0EF] px-[8px] py-[2px] text-[12px]/[140%]">
+        <p className="flex h-[21px] w-[24px] items-center justify-center rounded-[9999px] border border-[#DDE9E7] bg-[#E8F0EF] px-[8px] py-[2px] text-[12px]/[140%]">
           50
         </p>
       </div>
