64495cc4e9bc7c1c21fe52298b21152b56e578ae
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-17 12:06:55 +0000

    Correct tag list overflow behaviour

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -17,11 +17,11 @@ const TagElements = allUniqueTags.map((element, index) => (
 function SideBar() {
   return (
     <>
-      <aside className="row-start-1 row-end-3 hidden h-screen flex-col gap-[16px] border-r border-[#DDE9E7] lg:flex">
+      <aside className="col-start-1 col-end-2 row-start-1 row-end-3 hidden h-screen flex-col gap-[16px] border-r border-[#DDE9E7] lg:flex">
         <div className="px-[20px] pt-[20px] pb-[10px]">
           <img src="/img/logo-light-theme.svg" alt="" />
         </div>
-        <section className="flex flex-col gap-[16px] px-[16px] pt-[0px] pb-[20px]">
+        <section className="flex min-h-[0px] grow flex-col gap-[16px] px-[16px] pt-[0px] pb-[20px]">
           <div className="h-[84px] w-[264px]">
             <button className="flex h-[38px] w-full items-center gap-[12px] rounded-[6px] border-[#E8F0EF] bg-[#E8F0EF] px-[8px] px-[12px]">
               <img src="/img/icon-home.svg" alt="" />
@@ -32,12 +32,12 @@ function SideBar() {
               <p className="font-manrope">Archived</p>
             </button>
           </div>
-          <div>
+          <div className="flex grow flex-col">
             <div className="flex justify-between px-[12px] text-[12px]/[140%]">
               <p>TAGS</p>
               <p>Reset</p>
             </div>
-            <div>
+            <div className="h-[200px] grow overflow-auto">
               {TagElements}
               {/* <TagComponent />
               <TagComponent />
