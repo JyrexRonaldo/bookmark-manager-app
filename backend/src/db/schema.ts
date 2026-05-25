07910db40004e12e5f678c44625e29d2f56ebd68
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-05-25 09:14:38 +0000

    Set up database schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,4 +1,12 @@
-// import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  boolean,
+  text,
+  timestamp,
+  primaryKey,
+} from "drizzle-orm/pg-core";
 
 // export const usersTable = pgTable("users", {
 //   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,3 +14,40 @@
 //   age: integer().notNull(),
 //   email: varchar({ length: 255 }).notNull().unique(),
 // });
+
+export const bookmarksTable = pgTable("bookmarks", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  url: varchar({ length: 255 }).notNull(),
+  favicon: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+  tags: text().notNull(),
+  pinned: boolean(),
+  isArchived: boolean(),
+  visitCount: integer().notNull(),
+  createdAt: timestamp().defaultNow(),
+  lastVisited: timestamp(),
+});
+
+export const tagsTable = pgTable("tags", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+});
+
+export const bookmarksTagsTable = pgTable(
+  "bookmarks_Tags",
+  {
+    bookmarkId: integer("bookmark_id"),
+    tagId: integer("tag_id"),
+  },
+  (table) => [primaryKey({ columns: [table.bookmarkId, table.tagId] })],
+);
+
+// export const booksToAuthors = pgTable("books_to_authors", {
+//   authorId: integer("author_id"),
+//   bookId: integer("book_id"),
+// }, (table) => [
+//   primaryKey({ columns: [table.bookId, table.authorId] }),
+//   // Or PK with custom name
+//   primaryKey({ name: 'custom_name', columns: [table.bookId, table.authorId] }),
+// ]);
