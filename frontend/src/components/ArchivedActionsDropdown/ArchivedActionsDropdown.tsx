4252df21ceebc4737be19666e4bc483b2986a451
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-09-01 15:33:59 +0000

    Update dropdown to use headless ui dropdown

--- frontend/src/components/ArchivedActionsDropdown/ArchivedActionsDropdown.tsx
+++ frontend/src/components/ArchivedActionsDropdown/ArchivedActionsDropdown.tsx
@@ -1,26 +1,49 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
 function ArchivedActionsDropdown() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center bg-[#131313]/70">
-        <div className="flex h-[172px] w-[200px] flex-col gap-[4px] rounded-[8px] bg-white p-[8px] font-manrope *:flex *:h-[36px] *:items-center *:gap-[10px] *:p-[8px] *:text-[14px]/[140%]">
-          <div>
-            <img src="/img/icon-visit.svg" alt="" />
-            <p>Visit</p>
+      <Menu>
+        <MenuButton>
+          <button
+            type="button"
+            className="flex size-[32px] shrink-0 items-center justify-center rounded-[8px] border border-[#C0CFCC] hover:bg-[#E8F0EF] focus:bg-[#E8F0EF] focus:outline-4 focus:outline-[#E8F0EF]"
+          >
+            <img src="/img/icon-menu-bookmark.svg" alt="" />
+          </button>
+        </MenuButton>
+        <MenuItems
+          anchor="bottom end"
+          className="border-none outline-none focus:outline-none active:border-none active:outline-none"
+        >
+          <div className="flex h-[172px] w-[200px] flex-col gap-[4px] rounded-[8px] bg-white p-[8px] font-manrope *:flex *:h-[36px] *:items-center *:gap-[10px] *:p-[8px] *:text-[14px]/[140%]">
+            <MenuItem>
+              <div>
+                <img src="/img/icon-visit.svg" alt="" />
+                <p>Visit</p>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div>
+                <img src="/img/icon-copy.svg" alt="" />
+                <p>Copy URL</p>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div>
+                <img src="/img/icon-unarchive.svg" alt="" />
+                <p>Unarchive</p>
+              </div>
+            </MenuItem>
+            <MenuItem>
+              <div>
+                <img src="/img/icon-delete.svg" alt="" />
+                <p>Delete Permanently</p>
+              </div>
+            </MenuItem>
           </div>
-          <div>
-            <img src="/img/icon-copy.svg" alt="" />
-            <p>Copy URL</p>
-          </div>
-          <div>
-            <img src="/img/icon-unarchive.svg" alt="" />
-            <p>Unarchive</p>
-          </div>
-          <div>
-            <img src="/img/icon-delete.svg" alt="" />
-            <p>Delete Permanently</p>
-          </div>
-        </div>
-      </div>
+        </MenuItems>
+      </Menu>
     </>
   );
 }
